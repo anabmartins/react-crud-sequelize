f415a89059b5f500786ddcab169ccb239e59e0cf
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-05-19 00:42:07 -0300

    new

---
+++ src/components/TodoItem/style.js
@@ -0,0 +1,8 @@
+import { styles } from './styles';
+import { Text, TextInput, View, TouchableOpacity, FlatList, Alert } from 'react-native';
+import { Todo } from '../../components/TodoItem';
+import { useState } from 'react';
+
+export default function Home(){
+  
+}
