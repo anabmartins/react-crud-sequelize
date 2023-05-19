529abcd24a9643227bdafbe73b61682a5f4a606f
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-05-19 01:01:13 -0300

    socorr

---
+++ src/screens/Home/index.js
@@ -0,0 +1,4 @@
+import { styles } from './styles';
+import { Text, TextInput, View, TouchableOpacity, FlatList, Alert} from 'react-native';
+import { Participant } from '../../components/Participant';
+import { useState } from 'react';
